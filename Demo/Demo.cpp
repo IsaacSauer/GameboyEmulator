e6178e1446468563b0d91af3f10c1a4f52363268
Isaac Sauer <isaacsauer@hotmail.be>
2022-01-24 22:42:35 +0100

    Added more options to the options tab

--- Demo/Demo.cpp
+++ Demo/Demo.cpp
@@ -32,6 +32,90 @@ int gSpeedModifiers{ 1 };
 Measure gMeasure{ "timing" };
 bool gStarted = false;
 std::vector<uint64_t> gMsPerFrame{};
+std::string gCurrentFile{};
+
+void save_texture(SDL_Renderer* ren, SDL_Texture* tex, const char* filename)
+{
+	//Source: https://stackoverflow.com/a/48176678
+	SDL_Texture* ren_tex;
+	SDL_Surface* surf;
+	int st;
+	int w;
+	int h;
+	int format;
+	void* pixels;
+
+	pixels = NULL;
+	surf = NULL;
+	ren_tex = NULL;
+	format = SDL_PIXELFORMAT_RGBA32;
+
+	/* Get information about texture we want to save */
+	st = SDL_QueryTexture(tex, NULL, NULL, &w, &h);
+	if (st != 0) {
+		SDL_Log("Failed querying texture: %s\n", SDL_GetError());
+		goto cleanup;
+	}
+
+	ren_tex = SDL_CreateTexture(ren, format, SDL_TEXTUREACCESS_TARGET, w, h);
+	if (!ren_tex) {
+		SDL_Log("Failed creating render texture: %s\n", SDL_GetError());
+		goto cleanup;
+	}
+
+	/*
+	 * Initialize our canvas, then copy texture to a target whose pixel data we
+	 * can access
+	 */
+	st = SDL_SetRenderTarget(ren, ren_tex);
+	if (st != 0) {
+		SDL_Log("Failed setting render target: %s\n", SDL_GetError());
+		goto cleanup;
+	}
+
+	SDL_SetRenderDrawColor(ren, 0x00, 0x00, 0x00, 0x00);
+	SDL_RenderClear(ren);
+
+	st = SDL_RenderCopy(ren, tex, NULL, NULL);
+	if (st != 0) {
+		SDL_Log("Failed copying texture data: %s\n", SDL_GetError());
+		goto cleanup;
+	}
+
+	/* Create buffer to hold texture data and load it */
+	pixels = malloc(w * h * SDL_BYTESPERPIXEL(format));
+	if (!pixels) {
+		SDL_Log("Failed allocating memory\n");
+		goto cleanup;
+	}
+
+	st = SDL_RenderReadPixels(ren, NULL, format, pixels, w * SDL_BYTESPERPIXEL(format));
+	if (st != 0) {
+		SDL_Log("Failed reading pixel data: %s\n", SDL_GetError());
+		goto cleanup;
+	}
+
+	/* Copy pixel data over to surface */
+	surf = SDL_CreateRGBSurfaceWithFormatFrom(pixels, w, h, SDL_BITSPERPIXEL(format), w * SDL_BYTESPERPIXEL(format), format);
+	if (!surf) {
+		SDL_Log("Failed creating new surface: %s\n", SDL_GetError());
+		goto cleanup;
+	}
+
+	/* Save result to an image */
+	st = SDL_SaveBMP(surf, filename);
+	if (st != 0) {
+		SDL_Log("Failed saving image: %s\n", SDL_GetError());
+		goto cleanup;
+	}
+
+	SDL_Log("Saved texture as BMP to \"%s\"\n", filename);
+
+cleanup:
+	SDL_FreeSurface(surf);
+	free(pixels);
+	SDL_DestroyTexture(ren_tex);
+}
 
 int GetOpenGLDriverIndex()
 {
@@ -170,7 +254,11 @@ void Update(gbee::Emulator& emu)
 					emu.LoadGame(path);
 					emu.Start();
 				}
+
+				gCurrentFile = path.substr(path.find_last_of("/\\") + 1);
 			}
+			ImGui::SameLine();
+			ImGui::TextWrapped(gCurrentFile.c_str());
 
 			if (ImGui::Button("Toggle Paused", ImVec2{ 100, 20 }))
 				emu.SetPauseState(!emu.GetPauseState(0), 0);
@@ -180,26 +268,44 @@ void Update(gbee::Emulator& emu)
 			ImGui::SameLine();
 			ImGui::SliderInt("Speed", &speedModifiers, 1, 100);
 
-			////COLOR
-			//ImGui::Spacing();
-			//if (ImGui::TreeNode("Color 0:"))
-			//{
-			//	static ImVec4 color = ImVec4(114.0f / 255.0f, 144.0f / 255.0f, 154.0f / 255.0f, 200.0f / 255.0f);
-			//	ImGuiColorEditFlags misc_flags = flags;
-			//	flags |= ImGuiColorEditFlags_NoAlpha;        // This is by default if you call ColorPicker3() instead of ColorPicker4()
-			//	flags |= ImGuiColorEditFlags_AlphaBar;
-			//	flags |= ImGuiColorEditFlags_PickerHueBar;
-			//	flags |= ImGuiColorEditFlags_PickerHueWheel;
-			//	flags |= ImGuiColorEditFlags_NoInputs;       // Disable all RGB/HSV/Hex displays
-			//	flags |= ImGuiColorEditFlags_DisplayRGB;     // Override display mode
-			//	flags |= ImGuiColorEditFlags_DisplayHSV;
-			//	flags |= ImGuiColorEditFlags_DisplayHex;
-			//	if (ImGui::ColorPicker4("Color", (float*)&color, misc_flags, nullptr))
-			//	{
-			//		emu.SetColor0((float*)&color);
-			//	}
-			//	ImGui::TreePop();
-			//}
+			ImGui::Spacing();
+
+			//Screenshot
+			if (ImGui::Button("Save Screenshot...", ImVec2{ 150, 20 }))
+			{
+				emu.SetPauseState(true, 0);
+				std::string path{};
+
+				if (SaveFileDialog(path))
+				{
+					save_texture(gRendr, gTextures[0], path.c_str());
+				}
+
+				emu.SetPauseState(false, 0);
+			}
+
+			//COLOR
+			ImGui::Spacing();
+			if (ImGui::TreeNode("Colors:"))
+			{
+				{
+					static ImVec4 color = ImVec4(114.0f / 255.0f, 144.0f / 255.0f, 154.0f / 255.0f, 200.0f / 255.0f);
+					ImGuiColorEditFlags misc_flags = flags;
+					flags |= ImGuiColorEditFlags_NoAlpha;        // This is by default if you call ColorPicker3() instead of ColorPicker4()
+					flags |= ImGuiColorEditFlags_AlphaBar;
+					flags |= ImGuiColorEditFlags_PickerHueBar;
+					flags |= ImGuiColorEditFlags_PickerHueWheel;
+					flags |= ImGuiColorEditFlags_NoInputs;       // Disable all RGB/HSV/Hex displays
+					flags |= ImGuiColorEditFlags_DisplayRGB;     // Override display mode
+					flags |= ImGuiColorEditFlags_DisplayHSV;
+					flags |= ImGuiColorEditFlags_DisplayHex;
+					if (ImGui::ColorPicker4("Color", (float*)&color, misc_flags, nullptr))
+					{
+						emu.SetColor0((float*)&color);
+					}
+					ImGui::TreePop();
+				}
+			}
 
 			ImGui::End();
 			emu.SetSpeed(speedModifiers, 0);
@@ -263,7 +369,7 @@ int main(int argc, char* argv[])
 
 			emum.Start();
 
-			std::string base_filename = path.substr(path.find_last_of("/\\") + 1);
+			gCurrentFile = path.substr(path.find_last_of("/\\") + 1);
 
 			SDL_RenderClear(gRendr);
 			SDL_SetRenderDrawColor(gRendr, 114 - 50, 144 - 50, 154 - 50, 255);
